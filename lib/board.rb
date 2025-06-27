f017cca4f35effcf211bf385e1478e60a1da4b2a
Juan L C <202941136+jlcarg@users.noreply.github.com>
2025-06-27 17:13:38 +0900

    Add first playable version of game

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,14 @@
 class Board
   MIN_LETTERS = 5
   MAX_LETTERS = 12
-  attr_accessor :secret_word, :guessed_letters
+  CHANCES = 7
+  attr_accessor :secret_word, :guessed_letters, :chances, :correctly_guessed_letters, :incorrectly_guessed_letters
 
   def initialize
     @secret_word = select_secret_word
-    @guessed_letters = []
+    @chances = CHANCES
+    @correctly_guessed_letters = Array.new(@secret_word.length, '_')
+    @incorrectly_guessed_letters = []
   end
 
   def filter_words
@@ -15,7 +18,7 @@ class Board
   end
 
   def select_secret_word
-    @secret_word = filter_words.sample
+    @secret_word = filter_words.sample.chomp.upcase.split('')
   end
 
   def already_guessed?(letter)
@@ -30,16 +33,20 @@ class Board
     @secret_word.count letter
   end
 
+  def update_correctly_guessed!(letter_guessed)
+    @secret_word.each_with_index do |letter, idx|
+      @correctly_guessed_letters[idx] = letter_guessed if letter == letter_guessed
+    end
+  end
+
   def handle_player_guess(letter)
-    if already_guessed? letter
-      puts "You have already guessed #{letter}, try another letter!"
+    if letter_appears? letter
+      puts "You got a hit! #{letter} appears #{count_letter_appearances letter} time(s) in the secret word!"
+      update_correctly_guessed! letter
     else
-      @guessed_letters << letter
-      if letter_appears? letter
-        puts "You got a hit! #{letter} appears #{count_letter_appearances letter} time(s) in the secret word!"
-      else
-        puts "Unfortunately, #{letter} doesn't appear in the secret word!"
-      end
+      puts "Unfortunately, #{letter} doesn't appear in the secret word!"
+      @incorrectly_guessed_letters << letter
+      @chances -= 1
     end
   end
 end
--- lib/game.rb
+++ lib/game.rb
@@ -2,17 +2,38 @@ require_relative 'board'
 require_relative 'player'
 
 class Game
+  attr_accessor :board, :player
+
   def initialize
     @board = Board.new
     @player = Player.new
+    p @board.secret_word
   end
 
   def play
-    puts @board.secret_word
     @player.take_guess
-    @board.handle_player_guess @player.guessed_letters[-1]
+    @board.handle_player_guess(@player.guessed_letters[-1])
+    p @board.chances
+  end
+
+  def game_over?
+    @board.secret_word == @board.correctly_guessed_letters || @board.chances.zero?
+  end
+
+  def show_secret_word
+    puts "The secret word was: #{@board.secret_word}"
+  end
+
+  def start
+    play until game_over?
+    if @board.chances.zero?
+      puts "You Lose!\n"
+    else
+      puts "Congratulations! You Won!\n"
+    end
+    show_secret_word
   end
 end
 
 game = Game.new
-game.play
+game.start
